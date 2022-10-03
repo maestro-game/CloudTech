9187ba881afbb48accb1e57e3c9a3de0c4b16403
Vadim <vadim.computer.computer@gmail.com>
2022-10-03 16:16:44 +0300

    + init command realization

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "ru.itis"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("org.json:json:20220320")
+    implementation("com.amazonaws:aws-java-sdk-ec2:1.12.314")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.314")
+    implementation("com.amazonaws:aws-java-sdk-dynamodb:1.12.314")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,66 @@
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.HeadBucketRequest
+import org.json.JSONObject
+import java.io.File
+
+const val rs = "\u001B[0m"
+const val rd = "\u001B[31m"
+const val gr = "\u001B[32m"
+const val yl = "\u001B[33m"
+const val bl = "\u001B[34m"
+const val cn = "\u001B[36m"
+
+fun main(args: Array<String>) {
+    val iterator = args.iterator()
+    when (val param = iterator.next()) {
+        "upload" -> {}
+        "download" -> {}
+        "list" -> {}
+        "delete" -> {}
+        "mksite" -> {}
+        "init" -> init()
+        else -> { println("${rd}Unknown parameter$rs '$param'"); return }
+    }
+}
+
+fun init() {
+    println("${cn}Enter aws_access_key_id:$rs")
+    val key = readln()
+    println("${cn}Enter aws_secret_access_key:$rs")
+    val secret = readln()
+    println("${cn}Enter bucket:$rs")
+    val bucket = readln()
+    val s3 = AmazonS3ClientBuilder.standard()
+        .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(key, secret)))
+        .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("storage.yandexcloud.net","ru-central1")
+    ).build()
+    try {
+        s3.headBucket(HeadBucketRequest(bucket))
+        println("${gr}Bucket was found$rs")
+    } catch (e: AmazonServiceException) {
+        if (e.statusCode == 403) {
+            println("${rd}You does not have access to this bucket$rs")
+            return
+        }
+        if (e.statusCode == 301) {
+            println("${rd}This bucket is in a different region than the client is configured with$rs")
+            return
+        }
+        if (e.statusCode == 404) {
+            s3.createBucket(bucket)
+            println("${gr}Bucket was created$rs")
+        } else {
+            throw e
+        }
+    }
+
+    val json = JSONObject()
+    json.put("aws_access_key_id", key)
+    json.put("aws_secret_access_key", secret)
+    json.put("bucket", bucket)
+    File("config.txt").writeText(json.toString())
+}
